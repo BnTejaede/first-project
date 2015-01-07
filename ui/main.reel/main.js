9dcb8bedda403a130a10215ce17d494cfb3dcef1
BnTejaede <noreply>
2015-01-07 00:59:26 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -14,17 +14,17 @@ exports.Main = Component.specialize(/** @lends Main# */ {
             this.super();
         }
     },
-    defaultValue: { 
-        value:"World"
-    },
-
     handleIncrementAction: {
         value: function (event) {
+            var value = Number(this.templateObjects.value.element.textContent);
+            this.templateObjects.value.element.textContent = value + 1;
         }
     },
 
     handleDecrementAction: {
         value: function (event) {
+            var value = Number(this.templateObjects.value.element.textContent);
+            this.templateObjects.value.element.textContent = value - 1;
         }
     }
 
