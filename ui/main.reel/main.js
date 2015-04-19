03c7a9d01189ee289313fe5869cd94eeddf6ac1b
BnTejaede <noreply>
2015-04-19 22:33:49 +0000

    Update components

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -12,6 +12,13 @@ exports.Main = Component.specialize(/** @lends Main# */ {
     constructor: {
         value: function Main() {
             this.super();
+            var arrayForRangeController = [
+                {index:0,name:"Cell 1"},
+                {index:1,name:"Cell 2"},
+                {index:2,name:"Cell 3"},
+                {index:3,name:"Cell 4"},
+                {index:4,name:"Cell 5"},
+                ];
         }
     },
     handleIncrementAction: {
